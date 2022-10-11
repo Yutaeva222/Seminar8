edc49a9592c75984344c1d1223aa902ae435ec45
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-11 09:26:55 +0300

    задача53

---
+++ Task53/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет 
+// местами первую и последнюю строку массива.
+
+Console.WriteLine("Укажите размерность m вашего массива:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите размерность n вашего массива:  ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+ChangeArray(numbers);
+
+PrintArray(numbers);
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void ChangeArray(int[,] arr)
+{
+    int tempValue;
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            tempValue = arr[0,j];
+            arr[0,j] = arr[arr.GetLength(0)-1,j];
+            arr[arr.GetLength(0)-1,j] = tempValue;
+        }
+    }
+    Console.WriteLine();
+}
